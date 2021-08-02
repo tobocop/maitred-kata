d9da4e57fe3a7398d07b77123edca589136960b5
Toby Rumans <trumans@vmware.com>
2021-08-02 13:13:38 -0600

    initial commit

---
+++ src/main/java/maitredkata/Boutique.java
@@ -0,0 +1,4 @@
+package maitredkata;
+
+public class Boutique {
+}
---
+++ src/test/java/maitredkata/BoutiqueTest.java
@@ -0,0 +1,13 @@
+package maitredkata;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BoutiqueTest {
+    @Test
+    public void things() {
+        assertEquals(1, 2);
+    }
+}
